beb0b9c3696d7f3bd7ce39436417e853794795b2
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-23 02:06:45 +0530

    Create Area_of_Rectangle.cpp

---
+++ Area_of_Rectangle.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+int l,b,area=0;
+cout<<"Enter the breadth :";
+cin >> b;
+cout <<"Enter the length :";
+cin >> l;
+
+area = l * b;
+
+cout << "Area is :" << area  <<"\n";
+return 0;
+}
+
