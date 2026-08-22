a61c04cd91e3c19650e8f40ecb6532913a7874b6
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-23 02:13:35 +0530

    Create perimeter_of_rectangle.cpp

---
+++ perimeter_of_rectangle.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+int l,b,Perimeter=0;
+cout<<"Enter the breadth :";
+cin >> b;
+cout <<"Enter the length :";
+cin >> l;
+
+Perimeter = 2*(l + b);
+
+cout << "Perimeter  is :" << Perimeter  <<"\n";
+return 0;
+}
+
