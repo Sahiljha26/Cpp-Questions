617bd74897e79fcdc543f6c3ebe5474ba016e722
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-23 08:53:10 +0530

    Create Celsius_to_Fahrenheit.cpp

---
+++ Celsius_to_Fahrenheit.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+float celsius, fahrenheit;
+cout<<"Enter the temperature in celsius ; ";
+cin>>celsius;
+fahrenheit = (celsius *9/5) + 32;
+cout << "Temperature in Fahrenheit:" << fahrenheit <<"\n";
+return 0;
+}
+
