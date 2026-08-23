7bce523fc9e5b3c577e926b7775620d1a8b2e7d9
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-23 09:38:14 +0530

    Create ASCII_values.cpp

---
+++ ASCII_values.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+char ch ;
+cout<<"Enter a character : ";
+cin>> ch ;
+/*A-Z = 65-90
+  a-z = 97-122
+  0-9 = 48-57*/
+cout << "ASCII value :" << int(ch) <<"\n";
+return 0;
+}
+
