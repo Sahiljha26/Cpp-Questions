2be74ff127b192231898b624eed4c309eeac6aed
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-23 09:24:47 +0530

    Create Salary_assignment.cpp

---
+++ Salary_assignment.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main (){
+float salary;
+cout<<"Enter the Salary ";
+cin>>salary ;
+salary += 5000;
+cout << "Updated salary :" << salary  <<"\n";
+return 0;
+}
+
