76b774146e6ea2fc1a3571e56e4b9eebdd07869f
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-23 18:27:04 +0530

    Create Greatest_of_two_number.cpp

---
+++ Greatest_of_two_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int a,b ;
+cout<<"Enter a number  : ";
+cin>> a ;
+cout<<"Enter another  number  : ";
+cin>> b ;
+if (a>b){
+    cout<<"The greatest number is:"<<a<<"\n";
+} else {
+    cout<<"The greatest number is :"<<b<<"\n";
+}
+return 0;
+}
