c05bb9a777458613edd7b74dd0bcb673576894cd
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-26 21:52:51 +0530

    Create sum- of-first-n-natural-number.cpp

---
+++ sum- of-first-n-natural-number.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,sum=0;
+    cout<<"Enter the number:\n";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        sum+=i;
+    }
+    cout<<"sum ="<< sum << "\n";
+
+   
+    return 0;
+}
