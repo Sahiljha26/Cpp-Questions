853c58ba1e69ccd38e9926f3fc0a1ce15cf1fa67
Sahiljha26 <Sahilbela4@gmail.com>
2026-09-04 15:39:35 +0530

    Create pattern_space_pyramaid.cpp

---
+++ pattern_space_pyramaid.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:\n";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"0 ";
+        }
+        for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+        
+        
+    cout<<"\n";
+    }
+    
+    
+    return 0;
+}
